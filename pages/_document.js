1bc743b4ef95ce8fb6c9fbd881d4b397e51408b2
Nitin Tank <nitintank3@gmail.com>
2024-01-06 17:43:35 +0530

    Changes

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,16 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Link from 'next/link'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet' />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"/>
+        <Link rel="preconnect" href="https://fonts.googleapis.com" />
+        <Link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <Link href="https://fonts.googleapis.com/css2?family=Nunito:wght@500&display=swap" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />
