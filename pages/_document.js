1aa0373d46bf440d34ae9c256963053e705dcaec
Nitin Tank <nitintank3@gmail.com>
2024-01-09 11:12:53 +0530

    Changes

--- pages/_document.js
+++ pages/_document.js
@@ -11,17 +11,6 @@ export default function Document() {
         <Link rel="preconnect" href="https://fonts.googleapis.com" />
         <Link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <Link href="https://fonts.googleapis.com/css2?family=Nunito:wght@500&display=swap" rel="stylesheet" />
-        {/* <!-- Google tag (gtag.js) --> */}
-        <Script src="https://www.googletagmanager.com/gtag/js?id=G-ZCL1QE0P7K" />
-        <Script id="google-analytics">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
- 
-          gtag('config', 'G-ZCL1QE0P7K');
-        `}
-        </Script>
       </Head>
       <body>
         <Main />
