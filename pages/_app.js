ea57d97508a70a7341bf77642b6b8385e50ed1bc
Nitin Tank <nitintank3@gmail.com>
2024-01-23 12:22:21 +0530

    Landing Page Added

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,5 @@
 import '@/styles/globals.css'
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }) {
-
-  const router = useRouter()
-
-  useEffect(() => {
-    import('react-facebook-pixel')
-      .then((x) => x.default)
-      .then((ReactPixel) => {
-        ReactPixel.init('331707226505294') // facebookPixelId
-        ReactPixel.pageView()
-
-        router.events.on('routeChangeComplete', () => {
-          ReactPixel.pageView()
-        })
-      })
-  }, [router.events])
-
   return <Component {...pageProps} />
 }
