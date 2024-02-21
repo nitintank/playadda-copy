63141dae52a63d3b2e4750a9d738263e5d19593a
Nitin Tank <nitintank3@gmail.com>
2024-02-21 09:52:06 +0530

    Changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,6 @@
 import '@/styles/globals.css'
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }) {
-
-  const router = useRouter()
-
-  useEffect(() => {
-    import('react-facebook-pixel')
-      .then((x) => x.default)
-      .then((ReactPixel) => {
-        ReactPixel.init('916023883548010') // facebookPixelId
-        ReactPixel.pageView()
-
-        router.events.on('routeChangeComplete', () => {
-          ReactPixel.pageView()
-        })
-      })
-  }, [router.events])
 
   return <Component {...pageProps} />
 }
